16560dd0859539f28e1bdfb66154de8361d10ef5
GUERIN <34863367+folderguerin@users.noreply.github.com>
2017-12-27 16:20:45 +0100

    Update BlackToner.ps1

--- BlackToner.ps1
+++ BlackToner.ps1
@@ -3,4 +3,8 @@ $InitBlack = C:\SNMP\SnmpGet\SnmpGet.exe -q -r:192.168.0.65 -v:2c -c:public -o:.
 $RestBlack = C:\SNMP\SnmpGet\SnmpGet.exe -q -r:192.168.0.65 -v:2c -c:public -o:.1.3.6.1.2.1.43.11.1.1.9.1.4
 $ResultBlack = ($RestBlack / $InitBlack) * 100
 write-host $ResultBlack
+$InitCyan = C:\SNMP\SnmpGet\SnmpGet.exe -q -r:192.168.0.65 -v:2c -c:public -o:.1.3.6.1.2.1.43.11.1.1.8.1.1
+$RestCyan = C:\SNMP\SnmpGet\SnmpGet.exe -q -r:192.168.0.65 -v:2c -c:public -o:.1.3.6.1.2.1.43.11.1.1.9.1.1
+$ResultCyan = ($RestCyan / $InitCyan) * 100
+write-host $ResultCyan
 exit
